399695536a016b90228f96b2c8221bffbac9428a
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2021-05-23 15:52:47 +0200

    Update V1BO.py

--- V1BO.py
+++ V1BO.py
@@ -52,14 +52,19 @@ def center_and_surround_filter(kernelSize, sigmaI, sigmaO):
 # Von Mises distribition filter
 def von_mises_filter(kernelSize, radius, orientation):
     VMD = []
+    sigma = radius/2
     # create kernel grid
     for o in range(0, len(orientation)):
-        theta = np.deg2rad(orientation[o]+90)
+        orient = np.deg2rad(orientation[o]+90)
         # compute filter
         x, y = np.mgrid[:kernelSize, :kernelSize] - (kernelSize // 2)
-        numFilter = np.exp((np.sqrt(x**2+y**2)-radius)*np.sin(np.arctan2(y, x)-theta))
-        denumFilter = 2*np.pi*spherical_jn(0, np.sqrt(x**2+y**2))*(np.sqrt(x**2+y**2)-radius)
-        VMF = - (numFilter/denumFilter)
+        R = np.sqrt(x**2+y**2)
+        theta = np.arctan2(y, x)
+        VMF = np.exp(radius*np.cos(theta-(orient)))/np.i0(R-radius)
+        # normalize
+        VMF = VMF/np.max(np.max(VMF))
+        # append
+        VMD.append(VMF)
     return VMD
 
 # gabor filter bank generator
@@ -72,7 +77,7 @@ def gabor_filter_bank(kernelSize, wavelength, orientation):
     gammaValue = 0.3
     # compute orientation given the current scale
     for j in range(len(orientation)):
-        theta = np.deg2rad(orientation[j]+90)
+        theta = np.deg2rad(orientation[j])
         # compute standard filters
         x, y = np.mgrid[:kernelSize, :kernelSize] - (kernelSize // 2)
         rotx = x * np.cos(theta) + y * np.sin(theta)
@@ -133,125 +138,108 @@ def color_channel_processing(img):
     CC.append(CYB)
     return CC
 
-# Border Ownership grouping
-def border_ownership_processing():
-    # border ownership pyramid
-    BO = []
-    return BO
+# Flicker motion processing
+def flicker_processing(imgCur, imgPrev):
+    return cv2.absdiff(imgCur, imgPrev)
+
+# Motion orientation processing
+def motion_processing(orientCur, prevOrient):
+    return cv2.absdiff(orientCur, prevOrient)
 
 # get state
-img = cv2.imread("/home/main/Bureau/lenna.png")
-img = cv2.resize(img, (256,256))
+img = cv2.imread("/home/cyborg67/Bureau/cur.png")
+img = cv2.resize(img, (512,512))
 plt.matshow(img)
+plt.show()
+prevImg = cv2.imread("/home/cyborg67/Bureau/ref.png")
+prevImg = cv2.resize(prevImg, (512,512))
+plt.matshow(prevImg)
 plt.show()
 
 # create spatial pyramid
 imgPyr = ImagePyramid(img, 3)
+prevImgPyr = ImagePyramid(prevImg, 3)
 # normalize color image pyramid
 colorImgPyr = []
 for l in range(0,len(imgPyr)):
     colorImg = imgPyr[l] - np.mean(imgPyr[l])
     colorImg = colorImg / np.std(colorImg)
     colorImgPyr.append(colorImg)
-    """
-    plt.matshow(colorImg)
-    plt.show()
-    """
+
 # normalize gray image
 grayImgPyr = []
 for l in range(0,len(imgPyr)):
     grayImg = cv2.cvtColor(imgPyr[l], cv2.COLOR_BGR2GRAY) - np.mean(cv2.cvtColor(imgPyr[l], cv2.COLOR_BGR2GRAY))
     grayImg = grayImg / np.std(grayImg)
     grayImgPyr.append(grayImg)
-    """
-    plt.matshow(grayImg)
-    plt.show()
-    """
+
+# normalize previous gray image
+prevGrayImgPyr = []
+for l in range(0,len(prevImgPyr)):
+    prevGrayImg = cv2.cvtColor(prevImgPyr[l], cv2.COLOR_BGR2GRAY) - np.mean(cv2.cvtColor(prevImgPyr[l], cv2.COLOR_BGR2GRAY))
+    prevGrayImg = prevGrayImg / np.std(prevGrayImg)
+    prevGrayImgPyr.append(prevGrayImg)
+
 # LGN processing
 LGNImgPyr = []
 for l in range(0,len(grayImgPyr)):
     LGNImg = LGN_processing(grayImgPyr[l], sigmaPos=2, sigmaNeg=20)
     LGNImgPyr.append(LGNImg)
-    """
-    plt.matshow(LGNImg)
-    plt.show()
-    """
 
 # create gabor banks
-standardGaborBanks = gabor_filter_bank(kernelSize=5, wavelength=4, orientation=[0,45,90,135])
-dephasedGaborBanks = gabor_filter_bank(kernelSize=5, wavelength=4, orientation=[0+90,45+90,90+90,135+90])
-"""
-# simple gabor banks
-print("Simple Gabor cell")
-for i in range(0, len(standardGaborBanks)):
-    plt.matshow(standardGaborBanks[i])
-    plt.show()
-# complexe gabor banks
-print("dephased Gabor cell")
-for i in range(0, len(dephasedGaborBanks)):
-    plt.matshow(dephasedGaborBanks[i])
-    plt.show()
-"""
+standardGaborBanks = gabor_filter_bank(kernelSize=7, wavelength=4, orientation=[0,45,90,135])
+dephasedGaborBanks = gabor_filter_bank(kernelSize=7, wavelength=4, orientation=[90,135,180,225])
+
 # compute simple cell response at each scale of the image pyramid
 simpleCellPyr = []
 for l in range(0,len(grayImgPyr)):
     simpleCellPyr.append(simple_cell_processing(grayImgPyr[l], standardGaborBanks))
+# compute simple cell response at each scale of the image pyramid from the previous state
+prevSimpleCellPyr = []
+for l in range(0,len(prevGrayImgPyr)):
+    prevSimpleCellPyr.append(simple_cell_processing(prevGrayImgPyr[l], standardGaborBanks))
 # compute complexe cell response at each scale of the image pyramid
 complexeCellPyr = []
 for l in range(0,len(grayImgPyr)):
     complexeCellPyr.append(complex_cell_processing(grayImgPyr[l], standardGaborBanks, dephasedGaborBanks))
-"""
-# simple gabor response
-print("Simple Gabor response")
-for l in range(0, len(simpleCellPyr)):
-    for o in range(0, len(simpleCellPyr[0])):
-        plt.matshow(simpleCellPyr[l][o])
-        plt.show()
-# complexe gabor response
-print("Complexe Gabor response")
-for l in range(0, len(complexeCellPyr)):
-    for o in range(0, len(complexeCellPyr[0])):
-        plt.matshow(complexeCellPyr[l][o])
-        plt.show()
-"""
+
 # Compute color opponent response at each scale of the image pyramid
 colorOpponentPyr = []
 for l in range(0,len(colorImgPyr)):
     colorOpponentPyr.append(color_channel_processing(colorImgPyr[l]))
-"""
-# color opponent response
-print("Color opponent response")
-for l in range(0, len(colorOpponentPyr)):
-    for c in range(0, len(colorOpponentPyr[0])):
-        plt.matshow(colorOpponentPyr[l][c])
-        plt.show()
-"""
+
+# compute flicker response at each scale of the spatial pyramid
+flickerPyr = []
+for l in range(0, len(grayImgPyr)):
+    flickerPyr.append(flicker_processing(grayImgPyr[l], prevGrayImgPyr[l]))
+
+# compute motion response from spatial difference from 2 state image pyramid
+motionPyr = []
+for l in range(0, len(simpleCellPyr)):
+    orientDiff = []
+    for o in range(0, len(simpleCellPyr[0])):
+        orientDiff.append(motion_processing(simpleCellPyr[l][o], prevSimpleCellPyr[l][o]))
+    # append motion
+    motionPyr.append(orientDiff)
+
 # compute center and surround on edge, color and motion
 grayON = []
 grayOFF = []
 colorON = []
 colorOFF = []
-colorOpponentON = []
-colorOpponentOFF = []
 simpleCellON = []
 simpleCellOFF = []
-complexeCellON = []
-complexeCellOFF = []
+colorOpponentON = []
+colorOpponentOFF = []
+flickerCellON = []
+flickerCellOFF = []
+motionCellON = []
+motionCellOFF = []
 # compute at all level in the pyramid
 for l in range(0, len(colorImgPyr)):
     # basic color
     CON, COFF = center_and_surround(colorImgPyr[l], 5, 0.9, 2.7)
     GON, GOFF = center_and_surround(grayImgPyr[l], 5, 0.9, 2.7)
-    """
-    plt.matshow(CON)
-    plt.show()
-    plt.matshow(COFF)
-    plt.show()
-    plt.matshow(GON)
-    plt.show()
-    plt.matshow(GOFF)
-    plt.show()
-    """
     grayON.append(GON)
     grayOFF.append(GOFF)
     colorON.append(CON)
@@ -263,13 +251,6 @@ for l in range(0, len(colorImgPyr)):
     # CAS on color
     for c in range(0, len(colorOpponentPyr[0])):
         COCON, COCOFF = center_and_surround(colorOpponentPyr[l][c], 5, 0.9, 2.7)
-        """
-        plt.matshow(COCON)
-        plt.show()
-        plt.matshow(COCOFF)
-        plt.show()
-        """
-        # append current scale
         COON.append(COCON)
         COOFF.append(COCOFF)
     # append for all scale
@@ -282,12 +263,6 @@ for l in range(0, len(colorImgPyr)):
     # CAS on simple cell
     for o in range(0, len(simpleCellPyr[0])):
         SCOON, SCOOFF = center_and_surround(simpleCellPyr[l][o], 5, 0.9, 2.7)
-        """
-        plt.matshow(SCOON)
-        plt.show()
-        plt.matshow(SCOOFF)
-        plt.show()
-        """
         # append current scale
         SCON.append(SCOON)
         SCOFF.append(SCOOFF)
@@ -295,21 +270,29 @@ for l in range(0, len(colorImgPyr)):
     simpleCellON.append(SCON)
     simpleCellOFF.append(SCOFF)
 
-    # complexe cell
-    CCON = []
-    CCOFF = []
-    # CAS on complexe cell
-    for o in range(0, len(complexeCellPyr[0])):
-        CCOON, CCOOFF = center_and_surround(complexeCellPyr[l][o], 5, 0.9, 2.7)
-        """
-        plt.matshow(CCOON)
-        plt.show()
-        plt.matshow(CCOOFF)
-        plt.show()
-        """
-        # append current scale
-        CCON.append(CCOON)
-        CCOFF.append(CCOOFF)
+    # flicker cell
+    FCON, FCOFF = center_and_surround(flickerPyr[l], 5, 0.9, 2.7)
+    flickerCellON.append(FCON)
+    flickerCellOFF.append(FCOFF)
+
+    # motion cell
+    MCON = []
+    MCOFF = []
+    # CAS on simple cell
+    for m in range(0, len(motionPyr[0])):
+        MOON, MOOFF = center_and_surround(motionPyr[l][m], 5, 0.9, 2.7)
+        MCON.append(MOON)
+        MCOFF.append(MOOFF)
     # append for all scale
-    complexeCellON.append(CCON)
-    complexeCellOFF.append(CCOFF)
+    motionCellON.append(MCON)
+    motionCellOFF.append(MCOFF)
+
+# create Von mises filter banks
+vonMisesBanks = von_mises_filter(kernelSize=13, radius=2, orientation=[0,45,90,135])
+dephasedVonMisesBanks = von_mises_filter(kernelSize=13, radius=2, orientation=[180,45+180,90+180,135+180])
+
+for o in range(0, len(vonMisesBanks)):
+    plt.matshow(vonMisesBanks[o])
+    plt.show()
+    plt.matshow(dephasedVonMisesBanks[o])
+    plt.show()
